a4faa3fcb447628a02974b3eddbffee8e25d82dc
miguel-kjh <miguelangel.kjh@gmail.com>
2019-12-17 19:03:40 +0000

    changes

---
+++ webService/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'files_to_process'
+version = "1.0"
+ALLOWED_EXTENSIONS = set(['docx', 'pdf', 'xlsx', 'xlsm', 'xls', 'html'])
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 MB
---
+++ webService/cleanDBAndFile.py
@@ -0,0 +1,5 @@
+from ConnectionFileLog import ConnectionFileLog
+
+if __name__ == "__main__":
+    con = ConnectionFileLog()
+    con.deleteFile()

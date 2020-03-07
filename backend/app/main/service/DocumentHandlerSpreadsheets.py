7afe4abb3c5b2df96cdf5295ea5e1ff345a5363e
miguel-kjh <miguelangel.kjh@gmail.com>
2020-03-07 15:44:03 +0000

    multiple spreadsheets case implemented

--- backend/app/main/service/DocumentHandlerSpreadsheets.py
+++ backend/app/main/service/DocumentHandlerSpreadsheets.py
@@ -7,10 +7,61 @@ import pandas as pd
 
 
 class DocumentHandlerSpreadsheets(DocumentHandler):
-    def __init__(self, path: str, df:pd.DataFrame, destiny: str = ""):
+    def __init__(self, path: str, destiny: str = ""):
         super().__init__(path, destiny=destiny)
-        self.df = df
         self.selector = ColumnSelectorDataFrame()
+
+    def save(self):
+        pass
+
+class DocumentHandlerExcel(DocumentHandlerSpreadsheets):
+
+    def __init__(self, path: str, destiny: str = ""):
+        super().__init__(path, destiny=destiny)
+        self.sheets = pd.read_excel(path,sheet_name=None)
+
+    def giveListNames(self) -> tuple:
+        listNames = []
+        idCards   = []
+        for table in self.sheets:
+            for typeColumn in self.selector.getPossibleColumnsNames(self.sheets[table]):
+                if typeColumn.isName:
+                    dfNotNull = self.sheets[table][typeColumn.key][self.sheets[table][typeColumn.key].notnull()]
+                    countOfName = self.selector.columnSearch(dfNotNull,self.nameSearch.checkNameInDB)
+                    if countOfName / len(dfNotNull) > MEASURE_FOR_TEXTS_WITHOUT_CONTEXTS:
+                        listNames[len(listNames):] = dfNotNull
+                else:
+                    idCards[len(idCards):] = list(
+                        filter(lambda idCards: self.nameSearch.isDni(idCards),self.sheets[table][typeColumn.key][self.sheets[table][typeColumn.key].notnull()])
+                    )
+        return listNames,idCards
+
+    def documentsProcessing(self):
+        for table in self.sheets:
+            for typeColumn in self.selector.getPossibleColumnsNames(self.sheets[table]):
+                if typeColumn.isName:
+                    dfNotNull = self.sheets[table][typeColumn.key][self.sheets[table][typeColumn.key].notnull()]
+                    countOfName = self.selector.columnSearch(dfNotNull,self.nameSearch.checkNameInDB)
+                    if countOfName / len(dfNotNull) > MEASURE_FOR_TEXTS_WITHOUT_CONTEXTS:
+                        self.sheets[table][typeColumn.key].replace({str(name): encode(str(name)) for name in dfNotNull}, inplace=True)
+                else:
+                    idCards = list(
+                        filter(lambda idCards: self.nameSearch.isDni(idCards),self.sheets[table][typeColumn.key][self.sheets[table][typeColumn.key].notnull()])
+                    )
+                    self.sheets[table][typeColumn.key].replace({str(idCard): encode(str(idCard)) for idCard in idCards}, inplace=True)
+        self.save()
+    
+    def save(self):
+        writer = pd.ExcelWriter(self.destiny)
+        for sheetName in self.sheets:
+            self.sheets[sheetName].to_excel(writer, sheet_name = sheetName, index=False)
+        writer.save()
+
+class DocumentHandlerCsv(DocumentHandlerSpreadsheets):
+
+    def __init__(self, path: str, destiny: str = ""):
+        super().__init__(path, destiny=destiny)
+        self.df = pd.read_csv(path)
 
     def giveListNames(self) -> tuple:
         listNames = []
@@ -40,23 +91,6 @@ class DocumentHandlerSpreadsheets(DocumentHandler):
                 )
                 self.df[typeColumn.key].replace({str(idCard): encode(str(idCard)) for idCard in idCards}, inplace=True)
         self.save()
-
-    def save(self):
-        pass
-
-class DocumentHandlerExcel(DocumentHandlerSpreadsheets):
-
-    def __init__(self, path: str, destiny: str = ""):
-        super().__init__(path, pd.read_excel(path), destiny=destiny)
-
-
-    def save(self):
-        self.df.to_excel(self.destiny, index=False)
-
-class DocumentHandlerCsv(DocumentHandlerSpreadsheets):
-
-    def __init__(self, path: str, destiny: str = ""):
-        super().__init__(path, pd.read_csv(path), destiny=destiny)
 
 
     def save(self):

419a689468cae7a5e4df78dee62643c62326a3cf
miguel-kjh <miguelangel.kjh@gmail.com>
2020-03-19 16:11:12 +0000

    pdf read update

--- frontend-test/test.py
+++ frontend-test/test.py
@@ -5,11 +5,11 @@ nIter = 10
 messure = 0
 listaNames = 'http://127.0.0.1:5000/search/file/extract-data/json'
 encode = 'http://127.0.0.1:5000/search/file/encode'
-file = '/home/miguel/Escritorio/Ingeniería informática/cuarto/TFG/NameSearcher-WebService/frontend-test/demos/cementerio de los libros olvidados.txt'
+file = '/home/miguel/Escritorio/Ingeniería informática/cuarto/TFG/NameSearcher-WebService/frontend-test/demos/cementerio de los libros olvidados.pdf'
 
 for _ in range(nIter):
     with open(file, 'rb') as f:
         st = time.time()
-        r = requests.post(listaNames, files={'file': f})
+        r = requests.post(encode, files={'file': f})
         messure += time.time() - st
 print(messure/nIter, "s")

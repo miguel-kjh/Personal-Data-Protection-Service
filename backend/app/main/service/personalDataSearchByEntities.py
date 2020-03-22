3b0260bde7cbaeddfc4b16f32b52bc90b780f801
miguel-kjh <miguelangel.kjh@gmail.com>
2020-03-22 17:47:43 +0000

    tokenizer improved

--- backend/app/main/service/personalDataSearchByEntities.py
+++ backend/app/main/service/personalDataSearchByEntities.py
@@ -12,13 +12,14 @@ class PersonalDataSearchByEntities(PersonalDataSearch):
         listNames = []
         idCards   = []
         nextLen   = 0
-        for tokenText in sent_tokenize(text ,language='spanish'):
+        for tokenText in sent_tokenize(text.replace('—', ',') ,language='spanish'):
             doc = self.nlp(tokenText)
             listNames[len(listNames):] = self.selectNames([
                 {"name": ent.text, "star_char": ent.start_char+nextLen, "end_char": ent.end_char+nextLen}
                 for ent in doc.ents if ent.label_ == 'PER'
             ])
             nextLen += len(tokenText) + 1
+            #print(listNames)
         idCards = [
             {"name": idCard.group(), "star_char": idCard.start(), "end_char": idCard.end()}
             for idCard in list(filter(lambda x: isDni(x.group()) , re.finditer(r'\d{2}.?\d{2}.?\d{2}.?\d{2}\s*\w',text)))

cd285722cb02d33daeb4fed1f108ce67238c0344
miguel-kjh <miguelangel.kjh@gmail.com>
2020-03-28 19:16:44 +0000

    performance measures started

---
+++ backend/app/test/testPerfomance.py
@@ -0,0 +1,99 @@
+from app.test.base import BaseTestCase
+from app.main.service.CreateDocumentHandler import getCreatorDocumentHandler
+from app.main.service.languageBuilder import LanguageBuilder
+
+import unittest
+import json 
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from nltk.tokenize import sent_tokenize
+
+
+def saveHeatmap(array:np.array,filename:str):
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    heatmap = sns.heatmap(array,
+            cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    figure = heatmap.get_figure()
+    figure.savefig(filename, dpi=400)
+
+
+
+class TestPerfomanceTables(BaseTestCase):
+    
+    def test_tables(self):
+        with open('app/test/data/titulares.json') as file:
+            data = json.load(file)
+        creator        = getCreatorDocumentHandler('app/test/data/titulares.xls','xls')
+        dh             = creator.create()
+        listNames,_    = dh.giveListNames()
+        hits           = 0
+        failures       = 0
+        falsePositives = 0
+        falseNegatives = 0
+        df             = pd.read_excel("app/test/data/titulares.xls")
+        for name in listNames:
+            if name in data["names"]:
+                hits += 1
+            else:
+                falsePositives += 1
+
+        for name in data["names"]:
+            if name not in listNames:
+                falseNegatives += 1
+
+        for key in df.keys():
+            for ele in df[key]:
+                if ele not in listNames:
+                    failures += 1
+
+        saveHeatmap(np.array([[hits,falsePositives],[falseNegatives,failures]]),'app/test/result/table.png')
+
+class TestPerfomanceTexts(BaseTestCase):
+    def test_text(self):
+        with open('app/test/data/narracion.json') as file:
+            data = json.load(file)
+
+        creator        = getCreatorDocumentHandler('app/test/data/narracion.txt','txt')
+        dh             = creator.create()
+        listNames,_    = dh.giveListNames()
+        hits           = 0
+        failures       = 0
+        falsePositives = 0
+        falseNegatives = 0
+
+        for name in listNames:
+            for nameData in data["names"]:
+                if name == nameData:
+                    hits += 1
+                    break
+
+        for name in listNames:
+            if name not in data["names"]:
+                falsePositives += 1
+
+        for name in data["names"]:
+            if name not in listNames:
+                falseNegatives += 1
+        
+        tokens = []
+        nlp = LanguageBuilder().getlanguage()
+        with open('app/test/data/narracion.txt', 'r',encoding='utf8') as file:
+            for line in file:
+                for phares in sent_tokenize(line.replace('—', ',') ,language='spanish'):
+                    tokens[len(tokens):] = nlp(phares)
+        
+        for token in tokens:
+            for ent in listNames:
+                if token not in nlp(ent):
+                    failures += 1
+                    break
+
+        saveHeatmap(np.array([[hits,falsePositives],[falseNegatives,failures]]),'app/test/result/text.png')
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()

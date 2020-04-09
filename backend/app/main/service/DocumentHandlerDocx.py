9eb9f114085d658078c3288505197a94e832462e
miguel-kjh <miguelangel.kjh@gmail.com>
2020-04-09 10:02:53 +0100

    update DH docx

--- backend/app/main/service/DocumentHandlerDocx.py
+++ backend/app/main/service/DocumentHandlerDocx.py
@@ -10,6 +10,7 @@ from docx.text.paragraph import Paragraph
 from docx.table import Table
 
 import re
+from typing import Text
 
 class DocumentHandlerDocx(DocumentHandler):
 
@@ -17,86 +18,42 @@ class DocumentHandlerDocx(DocumentHandler):
         super().__init__(path, destiny=destiny)
         self.document = docx.Document(self.path)
 
-    def _getPickerData(self, table: Table) -> DataPickerInTables:
-        namePicker = DataPickerInTables()
-        isLabels = True
-        for row in table.rows:
-            for index, cell in enumerate(row.cells):
-                for paragraph in cell.paragraphs:
-                    if isLabels:
-                        labels = list(
-                            filter(lambda x: LanguageBuilder().semanticSimilarity(str(paragraph.text),
-                                                                                  x) > MEASURE_TO_COLUMN_KEY_REFERS_TO_NAMES,
-                                   listOfVectorWords))
-                        if labels:
-                            namePicker.addIndexColumn(index)
-                    else:
-                        if namePicker.isColumnName(index) and paragraph.text.strip():
-                            namePicker.addName(index, paragraph.text)
-            isLabels = False
-        return namePicker
-
-    def _defineNamePicker(self, table: Table, picker: DataPickerInTables):
-        for row in table.rows:
-            for index, cell in enumerate(row.cells):
-                if picker.isColumnName(index):
-                    for paragraph in cell.paragraphs:
-                        if paragraph.text.strip():
-                            picker.addName(index, paragraph.text)
-    
-    def getIdCards(self,table: Table, picker: DataPickerInTables) -> list:
-        idCards = []
-        for row in table.rows:
-            for index, cell in enumerate(row.cells):
-                if not picker.isColumnName(index):
-                    for paragraph in cell.paragraphs:
-                        if self.dataSearch.isDni(paragraph.text):
-                            idCards.append(paragraph.text)
-        return idCards
-
     def documentsProcessing(self):
-        #LastIndexesColumn = []
-        data = []
-        data[len(data):],data[len(data):] = self.giveListNames()
+        def updateDocx(regex:str, text:Text) -> Text:
+            regexName = re.compile(regex)
+            return regexName.sub(lambda match: encode(match.group()), text)
+        data              = []
+        maxLength         = 4000
+        listNames,idCards = self.giveListNames()
+        listNames = list(set(listNames))
+        listNames.sort(
+                key=lambda value: len(value),
+                reverse=True
+            )
+        data[len(data):],data[len(data):] = listNames,idCards
         regex = '|'.join(data)
         regexName = re.compile(regex)
         for block in itemIterator(self.document):
             if isinstance(block, Paragraph):
-                #if LanguageBuilder().hasContex(block.text):
-                #    listNames,listIdCards = self.dataSearch.searchPersonalData(block.text)
-                #    for name in listNames:
-                #        regexName = re.compile(name['name'])
-                #        text = regexName.sub(encode(name['name']), block.text)
-                #        block.text = text
-                #    for idCard in listIdCards:
-                #        regexName = re.compile(idCard['name'])
-                #        text = regexName.sub(encode(idCard['name']), block.text)
-                #        block.text = text
-                #elif self.dataSearch.isName(block.text):
-                #    regexName = re.compile(block.text.strip())
-                #    text = regexName.sub(encode(block.text.strip()), block.text)
-                #    block.text = text
-                #else:
-                #    _,listIdCards = self.dataSearch.searchPersonalData(block.text)
-                #    for idCard in listIdCards:
-                #        regexName = re.compile(idCard['name'])
-                #        text = regexName.sub(encode(idCard['name']), block.text)
-                #        block.text = text
+                if len(data) > maxLength:
+                    intial = 0
+                    for numberRange in range(maxLength,len(data),maxLength):
+                        block.text = updateDocx('|'.join(data[intial:numberRange]), block.text)
+                        intial     = numberRange
+                    block.text = updateDocx('|'.join(data[intial:]), block.text)
+                else:
                     block.text = regexName.sub(lambda match: encode(match.group()), block.text)
             elif isinstance(block, Table):
-                #picker = self._getPickerData(block)
-                #if picker.getIndexesColumn():
-                #    LastIndexesColumn = picker.getIndexesColumn()
-                #    initialRow = 1
-                #elif LastIndexesColumn:
-                #    picker.addIndexesColumn(LastIndexesColumn)
-                #    self._defineNamePicker(block, picker)
-                #    initialRow = 0
-                #else:
-                #    continue
                 for row in block.rows:
                     for cell in row.cells:
-                        cell.text = regexName.sub(lambda match: encode(match.group()), cell.text)
+                        if len(data) > maxLength:
+                            intial = 0
+                            for numberRange in range(maxLength,len(data),maxLength):
+                                cell.text = updateDocx('|'.join(data[intial:numberRange]), cell.text)
+                                intial = numberRange
+                            cell.text = updateDocx('|'.join(data[intial:]), cell.text)
+                        else:
+                            cell.text = regexName.sub(lambda match: encode(match.group()), cell.text)
 
             else:
                 continue

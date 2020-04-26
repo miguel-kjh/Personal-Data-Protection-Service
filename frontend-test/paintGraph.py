de810e1afe83aae9115b09fa3a6c9a4a89fc19c6
miguel-kjh <miguelangel.kjh@gmail.com>
2020-04-26 12:03:11 +0100

    initial python program for plots

--- frontend-test/paintGraph.py
+++ frontend-test/paintGraph.py
@@ -1,11 +1,12 @@
 import pandas
 import statistics
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 def getExtration(df:pandas.DataFrame) -> tuple:
-    times = df.iloc[[1]].values
-    words = df.iloc[[3]].values
-    return times[0],words[0]
+    encode = df.iloc[[1]].values
+    extr   = df.iloc[[0]].values
+    words  = df.iloc[[3]].values
+    return encode[0],extr[0],words[0]
 
 def sortData(df:pandas.DataFrame) -> pandas.DataFrame:
     transpose = df.T
@@ -17,11 +18,29 @@ if __name__ == "__main__":
     print(df)
     df = sortData(df)
     print(df)
-    times,words = getExtration(df)
-    times.sort()
-    print(times, words)
-    print(statistics.stdev(times))
-    plt.plot(words,times)
-    plt.show()
+    encode,extr,words = getExtration(df)
+    #print(statistics.stdev(times))
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=words,
+        y=encode,
+        name="Anonimization"
+    ))
+
+
+    fig.add_trace(go.Scatter(
+        x=words,
+        y=extr,
+        name="Extraction"
+    ))
+
+    fig.update_layout(
+        title="Web times",
+        xaxis_title="Word",
+        yaxis_title="Times"
+    )
+
+    fig.show()
 
     
--- frontend-test/test.py
+++ frontend-test/test.py
@@ -1,14 +1,15 @@
 import requests
 import time
+import sys
 
-nIter        = 10
+iterations   = 10
 listaNames   = 'http://127.0.0.1:5000/search/file/extract-data/json'
 encode       = 'http://127.0.0.1:5000/search/file/encode'
 tables       = '../backend/app/test/data/tablas/tabla'
 text         = '../backend/app/test/data/textos/carta'
 web          = '../backend/app/test/data/web/web'
 
-def takeMesure(filename:str, operation:str) -> float:
+def takeMesure(filename:str, operation:str, nIter:int) -> float:
     messure  = 0
     for _ in range(nIter):
         with open(filename, 'rb') as f:
@@ -18,7 +19,7 @@ def takeMesure(filename:str, operation:str) -> float:
     return round(messure/nIter,3)
 
 import pandas as pd
-def main():
+def test_time():
     fileCsvNames = [('times_txt.csv',"%s.txt", text), ('times_docx.csv',"%s.docx", text),('times_pdf.csv',"%s.pdf",text),
     ('times_tables.csv',"%s.xls",tables),('times_web.csv',"%s.html",web)]
     for fileCsvName,operation,filename in fileCsvNames:
@@ -26,11 +27,33 @@ def main():
             "text_1":[],"text_2":[],"text_3":[],"text_4":[],"text_5":[],
             "text_6":[],"text_7":[],"text_8":[],"text_9":[],"text_10":[]
         }
-        for index in range(1,nIter+1):
+        for index in range(1,iterations+1):
             file  = filename + operation %(index)
-            df["text_%i" %index].append(takeMesure(file,listaNames) * 1000)
-            df["text_%i" %index].append(takeMesure(file,encode) * 1000)
+            df["text_%i" %index].append(takeMesure(file,listaNames,iterations) * 1000)
+            df["text_%i" %index].append(takeMesure(file,encode,iterations) * 1000)
         pd.DataFrame(df, columns=df.keys()).to_csv(fileCsvName, index=False)
 
+def get_time() -> dict:
+    nIter   = 100
+    txt     = sum([takeMesure(text   + "10.txt",encode,1)  for _ in range(nIter)])
+    pdf     = sum([takeMesure(text   + "10.pdf",encode,1)  for _ in range(nIter)])
+    docx    = sum([takeMesure(text   + "10.docx",encode,1) for _ in range(nIter)])
+    xls     = sum([takeMesure(tables + "9.xls",encode,1)   for _ in range(nIter)])
+    html    = sum([takeMesure(web + "2.html",encode,1)     for _ in range(nIter)])
+
+    df =  {
+        "txt"  : [txt,  txt/nIter],
+        "pdf"  : [pdf,  pdf/nIter],
+        "docx" : [docx, docx/nIter],
+        "xls"  : [xls,  xls/nIter],
+        "html" : [html,  html/nIter],
+    }
+    pd.DataFrame(df, columns=df.keys()).to_csv('times.csv', index=False)
+    return df
+
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        if sys.argv[1].strip() == 'test_time':
+            test_time()
+        elif sys.argv[1].strip() == 'get_time':
+            print(get_time())

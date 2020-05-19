d8d0447fb71bda2e8107883344f5bbad516b35e5
miguel-kjh <miguelangel.kjh@gmail.com>
2020-05-19 16:17:21 +0100

    update frontedn

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,6 +1,11 @@
+const server     = "http://127.0.0.1:5000";
+const fileFormat = /^(txt|pdf|xls|docx|xlsx|xlsm|html|csv)$/;
+const urlRegex   = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+
+
 $(document).ready(function () {
-    const server = "http://127.0.0.1:5000"
-    setInterval(setTime, 5);
+    setInterval(setTime, 500);
+    setInterval(CheckFileExtension, 10);
     function setTime() {
         switch ($("#sel").val()) {
             case "1":
@@ -17,6 +22,17 @@ $(document).ready(function () {
         }
     }
 
+    function CheckFileExtension() {
+        let ext = $("#customFile").val().split('.').pop();
+        if (ext.match(fileFormat)) {
+            $('.file_upload').prop('disabled', false);
+            $('#alert').text("");
+        } else {
+            $('.file_upload').prop('disabled', true);
+            $('#alert').text("* Sola estan permitidos los siguientes formatos: txt, pdf, xls, docx, xlsx, xlsm, html y csv");
+        }
+    }
+
     $(".custom-file-input").on("change", function() {
         var fileName = $(this).val().split("\\").pop();
         $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
@@ -24,3 +40,15 @@ $(document).ready(function () {
 
     $("#form_web").attr('action', server+'/search/file/operation-web');
 });
+
+function validateForm() {
+    let web = $("#url_web").val();
+    if (web.match(urlRegex)) {
+        $('#url_web').removeClass('error');
+        return true;
+    } else {
+        $('#url_web').addClass('error');
+        return false;   
+    }
+}
+
